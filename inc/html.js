3fdb4a6106c7c9988ebc06e9d2a09620240df094
Moein Qorbani <szmp.info@gmail.com>
2023-12-05 16:03:36 +0330

    ff

--- inc/html.js
+++ inc/html.js
@@ -1,9 +1,11 @@
 <script>
     window.addEventListener("load", function() {
-        document.getElementsByClassName(".city_categories .filter-option-inner-inner")[0].innerHTML = "کشور و شهر";
-        document.getElementsByClassName(".main-search-form li.selected .selected span.text")[0].innerHTML = "کشور و شهر";
-        x = document.querySelectorAll(".city_categories .filter-option-inner-inner")[0].innerHTML;
-        console.log(x);
+        const lbl_search = document.querySelectorAll(".city_categories .filter-option-inner-inner");
+        const lbl_inner_search = document.querySelectorAll(".main-search-form li.selected .selected span.text");
+        lbl_search[0].innerHTML = "کشور و شهر";
+        lbl_inner_search[0].innerHTML = "کشور و شهر";
+        // x = document.querySelectorAll(".city_categories .filter-option-inner-inner")[0].innerHTML;
+        console.log(lbl_search);
 
     });
 </script>
