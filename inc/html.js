c37affd750da44b7a78bb75498a4ac0a328918ab
Moein Qorbani <szmp.info@gmail.com>
2023-12-08 10:49:24 +0330

    ff

--- inc/html.js
+++ inc/html.js
@@ -110,12 +110,14 @@
     let time_last_run;
 
     function fn(time_get) {
-        // if(time_last_run == ""){time_last_run == 0;}
+        if (time_last_run == "") {
+            time_last_run == 0;
+        }
         var time_fn = new Date().getTime() / 1000;
         // console.log("in fn");
         // let if_time = time_fn - time_get;
         // console.log(if_time);
-        if (time_fn - time_last_run > 0.2) {
+        if (time_get - time_last_run > 0.2) {
             time_last_run = new Date().getTime() / 1000;
 
             console.log("ffff");
